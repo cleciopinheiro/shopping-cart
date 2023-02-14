f9a4a5be71058341a97cb1a80600558913d6c999
cleciopinheiro <cleciosantosdeveloper@gmail.com>
2023-02-14 19:35:29 -0300

    Feat: Requisitos 3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
-console.log(fetchProductsList('computador'));
+const products = document.querySelector('.products');
+
+const getProductApi = async () => {
+  const arrayObj = await fetchProductsList('computador');
+  const productsArray = arrayObj.forEach((product) => {
+    const element = createProductElement(product);
+    products.appendChild(element);
+  });
+  return productsArray;
+};
+getProductApi();
 document.querySelector('.cep-button').addEventListener('click', searchCep);
